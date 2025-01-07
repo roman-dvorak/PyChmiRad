5610f9dfa2220b0126ab61064152d9e0a019f2de
Roman Dvorak <romandvorak@mlab.cz>
2025-01-07 22:37:12 +0100

    Update data types definitions

--- examples/download.py
+++ examples/download.py
@@ -2,24 +2,27 @@ import os
 from datetime import datetime, timedelta
 from pychmirad import ChmiRad
 
-# Nastavení složky pro ukládání dat
-download_dir = os.path.expanduser("~/chmi_data")  # Uloží data do ~/chmi_data
-dataset_type = "maxz"  # Změňte na požadovaný typ dat
 
-# Inicializace objektu ChmiRad
+download_dir = os.path.expanduser("~/chmi_data")
+dataset_type = "maxz"  # set reuired data type
+
+# initialize ChmiRad class
 chmi_rad = ChmiRad(dataset_type=dataset_type, download_dir=download_dir)
 
-# Nastavení časového intervalu
-#latest_datetime = datetime.utcnow().replace(minute=0, second=0, microsecond=0)
-#start_datetime = latest_datetime - timedelta(minutes=60*5)
+# Set timerange
+start_datetime = datetime(2025, 1, 7, 8, 0, 0)
+latest_datetime = datetime(2025, 1, 7, 21, 0, 0)
 
-start_datetime = datetime(2025, 1, 7, 10, 0, 0)
-latest_datetime = datetime(2025, 1, 7, 15, 0, 0)
-
-# Stažení dat
+# Data download
 chmi_rad.download_data_range(start_datetime, latest_datetime)
 
-# Změna datasetu a stažení dalších dat
+# new dataset
+chmi_rad.set_dataset_type("maxz_png")
+chmi_rad.download_data_range(start_datetime, latest_datetime)
+
 chmi_rad.set_dataset_type("pseudocappi2km")
+chmi_rad.download_data_range(start_datetime, latest_datetime)
+
+chmi_rad.set_dataset_type("pseudocappi2km_png")
 chmi_rad.download_data_range(start_datetime, latest_datetime)
 
--- src/pychmirad/chmirad.py
+++ src/pychmirad/chmirad.py
@@ -1,76 +1,71 @@
 import requests
-import h5py
-import matplotlib.pyplot as plt
-import numpy as np
-from datetime import datetime, timedelta
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
 import os
+from datetime import datetime, timedelta
+import datetime as dt
 
 class ChmiRad:
     DATA_TYPES = {
-        # This is not fully correct.. Works only for some outputs
-
-        "echotop": "hdf5/T_PANV23_C_OKPR_{}.hdf",
-        "fct_maxz": "hdf5/T_PANV23_C_OKPR_{}.hdf",
-        "fct_pseudocappi2km": "hdf5/T_PANV23_C_OKPR_{}.hdf",
-        "maxz": "hdf5/T_PABV23_C_OKPR_{}.hdf",
-        "merge1h": "hdf5/T_PASV23_C_OKPR_{}.hdf",
-        "pseudocappi2km": "hdf5/T_PANV23_C_OKPR_{}.hdf",
-        "maxz_png": "hdf5/T_PANV23_C_OKPR_{}.png",
-        "echotop_png": "hdf5/T_PANV23_C_OKPR_{}.png",
-        "merge1h_png": "png/T_PANV23_C_OKPR_{}.png"
+        "maxz": {"filename": "T_PABV23_C_OKPR_{}.hdf", "date_format": "%Y%m%d%H%M%S", "path": "maxz/hdf5"},
+        "pseudocappi2km": {"filename": "T_PANV23_C_OKPR_{}.hdf", "date_format": "%Y%m%d%H%M%S", "path": "pseudocappi2km/hdf5"},
+        "pseudocappi2km_png": {"filename": "pacz2gmaps3.z_cappi020.{}.0.png", "date_format": "%Y%m%d.%H%M", "path": "pseudocappi2km/png"},
+        "merge1h": {"filename": "T_PASV23_C_OKPR_{}.hdf", "date_format": "%Y%m%d%H%M%S", "path": "merge1h/hdf5"},
+        "echotop": {"filename": "T_PADV23_C_OKPR_{}.hdf", "date_format": "%Y%m%d%H%M%S", "path": "echotop/hdf5"},
+        "fct_maxz": {"filename": "T_PABV23_C_OKPR_{}_fctff.hdf", "date_format": "%Y%m%d%H%M%S", "path": "fct_maxz/hdf5"},
+        "fct_pseudocappi2km": {"filename": "T_PANV23_C_OKPR_{}_fctff.hdf", "date_format": "%Y%m%d%H%M%S", "path": "fct_pseudocappi2km/hdf5"},
+        "maxz_png": {"filename": "pacz2gmaps3.z_max3d.{}.0.png", "date_format": "%Y%m%d.%H%M", "path": "maxz/png/"},
+        "merge1h_png": {"filename": "T_PASV23_C_OKPR_{}.png", "date_format": "%Y%m%d%H%M", "path": "merge1h/png"},
+        "fct_maxz_tar": {"filename": "T_PABV23_C_OKPR_{}.ft60s10.tar", "date_format": "%Y%m%d.%H%M", "path": "fct_maxz/hdf5"},
+        "fct_pseudocappi2km_tar": {"filename": "T_PANV23_C_OKPR_{}.ft60s10.tar", "date_format": "%Y%m%d.%H%M", "path": "fct_pseudocappi2km/hdf5"},
     }
     
+    BASE_URL = "https://opendata.chmi.cz/meteorology/weather/radar/composite/{}/{}"
+
     def __init__(self, dataset_type='pseudocappi2km', download_dir='tmp'):
+        if dataset_type not in self.DATA_TYPES:
+            raise ValueError(f"Unknown dataset type: {dataset_type}")
         self.dataset_type = dataset_type
-        self.base_url_template = "http://opendata.chmi.cz/meteorology/weather/radar/composite/{}/{}"
-        self.download_dir = download_dir
-        if not os.path.exists(self.download_dir):
-            os.makedirs(self.download_dir)
-    
+        self.download_dir = os.path.expanduser(download_dir)
+        os.makedirs(self.download_dir, exist_ok=True)
+
     def set_dataset_type(self, dataset_type):
         if dataset_type not in self.DATA_TYPES:
             raise ValueError(f"Unknown dataset type: {dataset_type}")
         self.dataset_type = dataset_type
         print(f"Dataset type set to: {dataset_type}")
     
-    @staticmethod
-    def format_datetime(dt):
-        return dt.strftime('%Y%m%d%H%M%S')
+    @classmethod
+    def get_available_datasets(cls):
+        """Returns a list of available dataset types."""
+        return list(cls.DATA_TYPES.keys())
     
-    @staticmethod
-    def round_down_to_nearest_five(dt):
-        minute = (dt.minute // 5) * 5
-        return dt.replace(minute=minute, second=0, microsecond=0)
-    
+    def format_datetime(self, dt):
+        return dt.strftime(self.DATA_TYPES[self.dataset_type]["date_format"])
+
     def get_file_url(self, data_datetime):
         formatted_datetime = self.format_datetime(data_datetime)
-        filename = self.DATA_TYPES[self.dataset_type].format(formatted_datetime)
-        return self.base_url_template.format(self.dataset_type, filename)
-    
+        filename = self.DATA_TYPES[self.dataset_type]["filename"].format(formatted_datetime)
+        path = self.DATA_TYPES[self.dataset_type]["path"]
+        return self.BASE_URL.format(path, filename), filename
+
     def download_data(self, data_datetime):
-        url = self.get_file_url(data_datetime)
-        filename = os.path.join(self.download_dir, os.path.basename(url))
-        
-        if not os.path.exists(filename):
-            print(f"Downloading data from: {url}")
+        url, filename = self.get_file_url(data_datetime)
+        filepath = os.path.join(self.download_dir, filename)
+
+        if not os.path.exists(filepath):
+            print(f"Downloading: {url}")
             response = requests.get(url)
             
             if response.status_code == 200:
-                with open(filename, 'wb') as file:
+                with open(filepath, 'wb') as file:
                     file.write(response.content)
-                print(f"File downloaded successfully, size: {len(response.content)} bytes")
+                print(f"Saved: {filepath}")
             else:
-                print(f"Failed to download file, status code: {response.status_code}")
-                raise Exception("Failed to download data")
+                print(f"Failed to download: {url}, status {response.status_code}")
+                raise Exception("Download error")
         else:
-            print(f"File already exists: {filename}")
-    
+            print(f"File already exists: {filepath}")
+
     def download_data_range(self, start_datetime, end_datetime, interval_minutes=10):
-        start_datetime = self.round_down_to_nearest_five(start_datetime)
-        end_datetime = self.round_down_to_nearest_five(end_datetime)
-        
         current_datetime = start_datetime
         while current_datetime <= end_datetime:
             self.download_data(current_datetime)
@@ -78,15 +73,19 @@ class ChmiRad:
 
 # Example usage:
 if __name__ == "__main__":
-    dataset_type = "maxz"  # Change to desired dataset type
-    download_dir = "data"  # Change to desired directory
-    
+    dataset_type = "maxz_png"  # Change to desired dataset type
+    download_dir = "~/chmi_data"  # Change to desired directory
+
     visualizer = ChmiRad(dataset_type=dataset_type, download_dir=download_dir)
-    
-    latest_datetime = datetime.utcnow().replace(minute=0, second=0, microsecond=0)
+
+    latest_datetime = datetime.now(dt.UTC).replace(second=0, microsecond=0)
     start_datetime = latest_datetime - timedelta(minutes=60)
-    visualizer.download_data_range(start_datetime, latest_datetime)
     
-    # Change dataset type dynamically
-    visualizer.set_dataset_type("maxz_png")
+    visualizer.download_data_range(start_datetime, latest_datetime, interval_minutes=5)
+
+    # Example: Change dataset type dynamically
+    visualizer.set_dataset_type("fct_maxz_tar")
     visualizer.download_data(latest_datetime)
+
+    # Get available dataset types
+    print("Available datasets:", ChmiRad.get_available_datasets())
